00e70171b9b8e3c2742e95917a0f60c9eae35fc5
FloresAlvaro <alvarofloresfarfan2802@gmail.com>
2026-07-09 10:25:22 -0400

    Add categories page and update navigation menu

---
+++ app/pages/categories/index.vue
@@ -0,0 +1,9 @@
+<script lang="ts"></script>
+
+<template>
+    <div>
+        <h1>Categorías</h1>
+    </div>
+</template>
+
+
---
+++ app/pages/products/index.vue
@@ -0,0 +1,9 @@
+<script lang="ts"></script>
+
+<template>
+    <div>
+        <h1>Productos</h1>
+    </div>
+</template>
+
+
